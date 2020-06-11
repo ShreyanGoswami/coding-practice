698d9fa05643b87d94ab3ea3f270e0558ff88ee1
ShreyanGoswami <overclockedgtx@gmail.com>
2020-06-11 18:22:56 +0530

    Added new problems

---
+++ color sort/main.py
@@ -0,0 +1,33 @@
+'''
+Given an array with n objects colored red, white or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white and blue.
+
+Here, we will use the integers 0, 1, and 2 to represent the color red, white, and blue respectively.
+'''
+from typing import List
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        start = 0
+        end = len(nums) - 1
+        index = 0
+        
+        while index <= end and start < end:
+            if nums[index] == 0:
+                nums[index] = nums[start]
+                nums[start] = 0
+                start += 1
+                index += 1
+            elif nums[index] == 2:
+                nums[index] = nums[end]
+                nums[end] = 2
+                end -= 1
+            else:
+                index += 1
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [2,0,2,1,1,0]
+    s.sortColors(nums)
+    print(nums)
---
+++ search insert position/main.py
@@ -0,0 +1,29 @@
+'''
+Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+You may assume no duplicates in the array.
+'''
+
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums)
+        while start < end:
+            mid = (start + end) >> 1
+            if nums[mid] == target:
+                return mid
+            elif mid!= 0 and nums[mid-1] < target < nums[mid]:
+                return mid
+            elif mid!= end-1 and nums[mid] < target < nums[mid+1]:
+                return mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+            elif nums[mid] < target:
+                start = mid + 1
+        return start
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.searchInsert([1,3,5,6,8], 9))
